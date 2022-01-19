847e2a07e20e0524821a638ee967ef6d9cbac40c
taro.kanda <ko0814chata@gmail.com>
2022-01-19 13:08:17 +0000

    0119kadai2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Task.create(content: 'test title 1', status: 'test 1')
+Task.create(content: 'test title 2', status: 'test 2')
+Task.create(content: 'test title 3', status: 'test 3')
