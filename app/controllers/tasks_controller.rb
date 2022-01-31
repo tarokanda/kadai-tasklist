e8ad96398a0341be950ff9a3601afbc456834b01
taro.kanda <ko0814chata@gmail.com>
2022-01-31 12:32:46 +0000

    kadai01312

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:read,:update,:destroy]
+  #before_action :correct_user, only: [:read,:update,:destroy]
 
 
   def index
